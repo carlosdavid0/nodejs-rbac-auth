3a6071a001502c731ed7c986f09f3d8dfe8535a0
Carlos David <romao.carlosdavi@gmail.com>
2025-01-17 09:14:30 -0300

    style: implementacao no eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,16 @@
-import globals from "globals";
 import pluginJs from "@eslint/js";
+import globals from "globals";
 import tseslint from "typescript-eslint";
-
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": ["off"],
+    },
+  },
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
