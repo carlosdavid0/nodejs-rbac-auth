40bc8372beae9b2f462e7b068c8205a3a8e36a2d
Carlos David <romao.carlosdavi@gmail.com>
2025-01-17 09:15:58 -0300

    feat: ajustando logica para controllers e implementando no sign in

---
+++ src/application/controllers/auth/SignInController.ts
@@ -0,0 +1,48 @@
+import { z, ZodError } from "zod";
+import { InvalidCredentials } from "../../errors/auth/InvalidCredentials";
+import { IController, IRequest, IResponse } from "../../interfaces/IController";
+import { SignIpUseCase } from "../../useCases/auth/signInUseCase";
+
+const schema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+});
+
+export class SignUpController implements IController {
+  constructor(private readonly signInUseCase: SignIpUseCase) {}
+  async handle({ body }: IRequest): Promise<IResponse> {
+    try {
+      const { email, password } = schema.parse(body);
+
+      const { accessToken } = await this.signInUseCase.execute({
+        email,
+        password,
+      });
+
+      return {
+        statusCode: 200,
+        body: { accessToken },
+      };
+    } catch (err) {
+      if (err instanceof ZodError) {
+        return {
+          statusCode: 400,
+          body: {
+            error: err.issues,
+          },
+        };
+      }
+
+      if (err instanceof InvalidCredentials) {
+        return {
+          statusCode: 401,
+          body: {
+            error: "Invalid credentials",
+          },
+        };
+      }
+
+      throw err;
+    }
+  }
+}
---
+++ src/application/useCases/auth/signInUseCase.ts
@@ -0,0 +1,38 @@
+import { compare } from "bcryptjs";
+import { InvalidCredentials } from "../../errors/auth/InvalidCredentials";
+import { prismaClient } from "../../lib/prismaClient";
+
+import { sign } from "jsonwebtoken";
+import { env } from "../../config/env";
+
+interface IInput {
+  email: string;
+  password: string;
+}
+
+interface IOutput {
+  accessToken: string;
+}
+export class SignIpUseCase {
+  async execute({ email, password }: IInput): Promise<IOutput> {
+    const account = await prismaClient.account.findUnique({
+      where: { email: email },
+    });
+
+    if (!account) {
+      throw new InvalidCredentials();
+    }
+
+    const isPasswordValid = await compare(password, account.password);
+
+    if (!isPasswordValid) {
+      throw new InvalidCredentials();
+    }
+
+    const accessToken = sign({ sub: account.id }, env.jwtSecret, {
+      expiresIn: "1d",
+    });
+
+    return { accessToken };
+  }
+}
