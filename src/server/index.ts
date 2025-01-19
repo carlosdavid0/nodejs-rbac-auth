25b8823adb4ac1be71b091a81f5de8674947e5d8
Carlos David <romao.carlosdavi@gmail.com>
2025-01-19 10:28:19 -0300

    feat: add routeAdapter with factory pattern

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,29 +2,14 @@ import express from "express";
 
 import { makeSignInController } from "../factories/auth/makeSignInController";
 import { makeSignUpController } from "../factories/auth/makeSignUpController";
+import { routeAdapter } from "./adapter/routeAdapter";
 
 const app = express();
 
 app.use(express.json());
 
-app.post("/sign-up", async (request, response) => {
-  const signUpController = makeSignUpController();
-
-  const { body, statusCode } = await signUpController.handle({
-    body: request.body,
-  });
-
-  response.status(statusCode).json(body);
-});
-app.post("/sign-in", async (request, response) => {
-  const signInController = makeSignInController();
-
-  const { body, statusCode } = await signInController.handle({
-    body: request.body,
-  });
-
-  response.status(statusCode).json(body);
-});
+app.post("/sign-up", routeAdapter(makeSignUpController()));
+app.post("/sign-in", routeAdapter(makeSignInController()));
 
 app.listen(3001, () => {
   console.log("Server is running on port 3001");
